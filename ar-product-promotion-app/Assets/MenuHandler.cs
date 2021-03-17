d13234bf93312db8c93c4a147a63f32ff609c2a8
MartienJun <martienjunaedi123@gmail.com>
2021-03-17 19:58:27 +0700

    Adding script for button in Start menu

---
+++ ar-product-promotion-app/Assets/MenuHandler.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuHandler : MonoBehaviour
+{
+    public GameObject AboutMenu; 
+    public GameObject ExitMenu;
+    public string UrlInstagram = "https://www.instagram.com/martienjunaedi/";
+    public string UrlGithub = "https://github.com/MartienJun";
+    public void ExitApp() 
+    { 
+        Application.Quit(); 
+        Debug.Log("Keluar..."); 
+    } 
+    
+    public void StartClicked(string scene_name) 
+    { 
+        SceneManager.LoadScene(scene_name); 
+    } 
+    
+    public void AboutClicked() 
+    {
+        AboutMenu.SetActive(true);
+        ExitMenu.SetActive(false);
+    }
+
+    public void ExitClicked()
+    {
+        AboutMenu.SetActive(false);
+        ExitMenu.SetActive(true);
+    }
+
+    public void BackClicked() 
+    {
+        AboutMenu.SetActive(false);
+        ExitMenu.SetActive(false);
+    }
+
+    public void InstagramClicked()
+    {
+        Application.OpenURL(UrlInstagram);
+    }
+
+    public void GithubClicked()
+    {
+        Application.OpenURL(UrlGithub);
+    }
+}
